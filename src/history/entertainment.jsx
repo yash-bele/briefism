367fd1844aa1b8fdc58264ea19bdebd0aeab4324
Yash Bele <yashgbele@gmail.com>
2024-01-11 13:22:06 +0530

    jan-11 entertainment 2

--- src/history/entertainment.jsx
+++ src/history/entertainment.jsx
@@ -1,6 +1,79 @@
 import Image from "next/image";
 
 export const entertainmentHistory = [
+  // jan-09-2024
+  {
+    id: "oppenheimers-triumph-at-the-2024-golden-globes-best-film-award-wins-cillian-murphy-christopher-nolan",
+    timestamp: "Jan 09, 2024",
+    image:
+      "/jan-09-2024/oppenheimers-triumph-at-the-2024-golden-globes-best-film-award-wins-cillian-murphy-christopher-nolan.jpg",
+    title: `Oppenheimer's Triumph at the 2024 Golden Globes: Best Film Award - Wins Cillian Murphy, Christopher Nolan`,
+    preArticle: `The 81st Golden Globes, which marked the beginning of this year's
+          Hollywood award season, were dominated by Oppenheimer and Succession.
+          Out of the eight nominations, Oppenheimer took home five wins,
+          including Best Film (Drama). In addition to winning Best Director,
+          Christopher Nolan's film also took home two major acting honors: for
+          his roles as Lewis Strauss and Robert Oppenheimer, Cillian Murphy won
+          Best Film Actor (Drama) and Robert Downey Jr. took home Best
+          Supporting Actor (Film). Ryan Gosling, who portrayed Ken in Barbie:
+          Barbie and Oppenheimer battled at the worldwide box office with
+          remarkable outcomes last year, was in RDJ's category. Best Score was
+          also won by Oppenheimer.`,
+    article: (
+      <div className="space-y-6">
+        <p>
+          The 81st Golden Globes, which marked the beginning of this year&apos;s
+          Hollywood award season, were dominated by Oppenheimer and Succession.
+          Out of the eight nominations, Oppenheimer took home five wins,
+          including Best Film (Drama). In addition to winning Best Director,
+          Christopher Nolan&apos;s film also took home two major acting honors:
+          for his roles as Lewis Strauss and Robert Oppenheimer, Cillian Murphy
+          won Best Film Actor (Drama) and Robert Downey Jr. took home Best
+          Supporting Actor (Film). Ryan Gosling, who portrayed Ken in Barbie:
+          Barbie and Oppenheimer battled at the worldwide box office with
+          remarkable outcomes last year, was in RDJ&apos;s category. Best Score
+          was also won by Oppenheimer.
+        </p>
+        <p>
+          In a surprising turn of events, Emma Stone won Best Film Actress
+          (Musical/Comedy), and Poor Things defeated Barbie for Best Film
+          (Musical/Comedy). Paul Giamatti and Da&apos;Vine Randolph, the stars
+          of The Holdovers, took home the Best Actor (Musical/Comedy) and Best
+          Supporting Actress film awards. The first-ever victory for an
+          Indigenous actor was achieved by Lily Gladstone, who won Best Film
+          Actress (Drama) for Killers of the Flower Moon.
+        </p>
+        <p>
+          Succession, The Bear, and Beef all won handily in their respective
+          categories. Roman Roy, portrayed by Kieran Culkin, is the Succession
+          character who would win Best TV Actor (Drama). Best Supporting Actor
+          (TV) and Best TV Actress (Drama) went to co-stars Sarah Snook and
+          Matthew Macfadyen. Best Series (Drama) went to Succession as well.
+        </p>
+        <p>
+          Beef took home the awards for Best Limited Series, Anthology, or TV
+          Movie in addition to Best Actress and Best Actor for Steven Yeun and
+          Ali Wong.
+        </p>
+        <p>
+          For the second consecutive year, Jeremy Allen White won Best TV Actor
+          (Musical/Comedy) for The Bear, while co-star Ayo Edebiri took home the
+          Best TV Actress (Musical/Comedy) award. Also awarded Best Series
+          (Musical/Comedy) was The Bear.
+        </p>
+        <p>
+          After several contentious years of criticism and a boycott of the
+          Hollywood Foreign Press Association, which presented the awards
+          previously, the Golden Globes are back this year under new ownership.
+          The production company that has created and aired the show for many
+          years now owns the Globes. This year&apos;s Golden Globes, which are
+          hosted by comedian Jo Koy, feature two new categories: Best
+          Performance in Stand-Up Comedy on TV and Cinematic and Box Office
+          Achievement.
+        </p>
+      </div>
+    ),
+  },
   // week-jan-02-2024
   {
     id: "captain-miller-trailer-shows-dhanush-battling-british-forces",
