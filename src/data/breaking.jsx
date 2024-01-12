6d62d93530dd17d5fb8111f7d096fc383a358d99
Yash Bele <yashgbele@gmail.com>
2024-01-12 11:17:03 +0530

    jan-12 breaking 2+2

--- src/data/breaking.jsx
+++ src/data/breaking.jsx
@@ -3,6 +3,165 @@ import Image from "next/image";
 export const breaking = [
   // jan-12-2024
   {
+    id: "massive-retaliatory-strike-against-iranian-backed-houthis-in-yemen-launched-by-us-and-british-militaries",
+    timestamp: "Jan 12, 2024",
+    image:
+      "/jan-12-2024/massive-retaliatory-strike-against-iranian-backed-houthis-in-yemen-launched-by-us-and-british-militaries.jpg",
+    title: `Massive Retaliatory Strike Against Iranian-backed Houthis in Yemen Launched by US and British Militaries`,
+    preArticle: `The U.S. and British militaries bombed over a dozen sites used by
+          Iran-backed Houthi rebels in Yemen on Thursday. The massive
+          retaliatory strike utilized warship and submarine missiles alongside
+          fighter jets to target air defenses, drone and missile storage, and
+          launching locations, officials said.`,
+    article: (
+      <div className="space-y-6">
+        <p>
+          The U.S. and British militaries bombed over a dozen sites used by
+          Iran-backed Houthi rebels in Yemen on Thursday. The massive
+          retaliatory strike utilized warship and submarine missiles alongside
+          fighter jets to target air defenses, drone and missile storage, and
+          launching locations, officials said.
+        </p>
+        <p>
+          President Biden stated the strikes demonstrated the U.S. and allies
+          &quot;will not tolerate&quot; the militant group&apos;s relentless
+          attacks on the Red Sea. Biden said they only acted after attempts at
+          diplomacy and careful consideration.
+        </p>
+        <p>
+          The strikes directly responded to unprecedented Houthi attacks on
+          international ships, including first-time use of anti-ship ballistic
+          missiles, endangering personnel and trade, Biden explained. He added
+          he will take further measures as needed to protect people and
+          commerce.
+        </p>
+        <p>
+          In Yemen&apos;s capital Sanaa, journalists heard four explosions early
+          Friday local time. Residents in the port city of Hodeida said they
+          heard five strong blasts. Witnesses also reported strikes in cities
+          south of Sanaa.
+        </p>
+        <p>
+          The coordinated assault marked the first U.S. military response to the
+          Houthis&apos; persistent attacks on commercial vessels since the
+          Israel-Hamas war began. It came just a week after the U.S. and
+          partners warned the Houthis to stop or risk potential military action.
+          The warning briefly deterred attacks before the rebels&apos;
+          largest-ever drone and missile barrage on shipping Tuesday, prompting
+          U.S. and British ships to shoot down missiles.
+        </p>
+        <p>
+          Officials said after Tuesday&apos;s attacks, Biden met with advisors
+          and directed the retaliatory strikes. The UK confirmed its Royal Air
+          Force took part, with fighters from Cyprus. Allied nations joined the
+          U.S. and UK in saying they won&apos;t hesitate to defend lives and
+          commerce, though the aim is de-escalation.
+        </p>
+        <p>
+          The Houthis vowed retaliation, warning any attack will spark a fierce
+          response beyond imagination. Houthi-run media described strikes
+          hitting bases, airports and camps. Casualties remain unclear.
+          Officials don&apos;t expect the strikes will eliminate Houthi
+          capabilities but may prompt retaliation.
+        </p>
+        <p>
+          The Houthis say their attacks aim to stop Israel&apos;s war in Gaza.
+          But their targets increasingly have little connection to Israel while
+          imperiling a crucial Asia-Europe trade route. The UN Security Council
+          passed a resolution Wednesday demanding the Houthis immediately halt
+          attacks and implicitly condemned their Iranian weapons suppliers.
+        </p>
+        <p>
+          The broad coalition battling the Houthis shows the Biden
+          administration&apos;s effort to avoid appearing unilateral. Multiple
+          ships have already been struck despite a U.S.-led maritime mission to
+          protect shipping. The U.S. long declined to signal when patience would
+          run out before Wednesday&apos;s warning of consequences.
+        </p>
+        <p>
+          The reluctance reflected sensitivities about the shaky Yemen truce and
+          worries about triggering wider regional conflict. But the
+          attacks&apos; impact on shipping and escalation triggered the
+          coalition warning. The strikes underscored using a united front rather
+          than opening a new war front alone.
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: "decision-by-grand-jury-ohio-woman-escapes-corpse-abuse-charges-after-home-miscarriage",
+    timestamp: "Jan 12, 2024",
+    image:
+      "/jan-12-2024/decision-by-grand-jury-ohio-woman-escapes-corpse-abuse-charges-after-home-miscarriage.webp",
+    title: `Decision by Grand Jury: Ohio Woman Escapes Corpse Abuse Charges After Home Miscarriage`,
+    preArticle: `An Ohio woman who was criminally charged for allegedly mishandling a
+          home miscarriage will not be indicted, a grand jury decided on
+          Thursday.`,
+    article: (
+      <div className="space-y-6">
+        <p>
+          An Ohio woman who was criminally charged for allegedly mishandling a
+          home miscarriage will not be indicted, a grand jury decided on
+          Thursday.
+        </p>
+        <p>
+          The prosecutor&apos;s office in Trumbull County said the grand jury
+          declined to charge Brittany Watts, 34, of Warren with abuse of a
+          corpse. This resolved a controversial case that drew national
+          attention for its implications for pregnant women as states enact new
+          laws on reproductive healthcare access post - Roe v. Wade.
+        </p>
+        <p>
+          The decision came just before around 150 supporters gathered for a
+          rally supporting Watts in Warren&apos;s Courthouse Square. Watts
+          addressed the crowd, thanking her community and declaring she will
+          continue fighting.
+        </p>
+        <p>
+          Watts&apos; attorney said an outpouring of public support helped her
+          client endure being charged with a felony. The lawyer stated that
+          despite how upsetting it sounds, Watts&apos; experience reflects the
+          sad reality of miscarriage. They expressed relief and gratitude that
+          the grand jury reached a just decision today.
+        </p>
+        <p>
+          A municipal judge previously found probable cause against Watts.
+          Prosecutors alleged she miscarried into a toilet, removed contents to
+          outdoor trash, and left the 22-week fetus lodged in pipes. Records
+          show her doctor advised hospital induction due to nonviable pregnancy
+          and serious risks. But complications and delays prevented treatment.
+          After miscarrying at home, she unsuccessfully tried going to a hair
+          appointment before friends insisted she go to the hospital. A
+          nurse&apos;s 911 call spurred the ensuing investigation.
+        </p>
+        <p>
+          The prosecutor argued the issue was not how or when the fetus died,
+          but that Watts discarded it in the toilet, clogging it, and went about
+          her day. An autopsy found the fetus died in utero with no recent
+          injuries. Watts&apos; lawyer contended she had no criminal record and
+          was being vilified for a common occurrence. The lawyer also questioned
+          whether the abuse-of-corpse statute clearly defined terms like
+          &quot;corpse&quot; and &quot;outrage&quot;.
+        </p>
+        <p>
+          After binding the case over, the judge acknowledged better scholars
+          should determine the fetus&apos;s precise legal status. Now, the
+          prosecutor says further investigation found Watts committed no crime,
+          despite disagreeing with the lower court&apos;s law application.
+        </p>
+        <p>
+          Advocates expressed relief at the outcome, but warned Watts&apos; case
+          exemplifies the threats Black women face for pregnancy outcomes amid
+          abortion restrictions. They called for ending the criminalization of
+          reproductive outcomes, which undermines rights and deters necessary
+          care. Watts hopes her experience prompts change through education and
+          legislation so no other woman endures similar trauma fighting for
+          freedom.
+        </p>
+      </div>
+    ),
+  },
+  {
     id: "love-and-betrayal-shoaib-sheikh-accused-of-bank-manager-amit-kaurs-killing",
     timestamp: "Jan 12, 2024",
     image:
