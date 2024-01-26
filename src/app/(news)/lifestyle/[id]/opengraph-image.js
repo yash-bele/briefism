02e0d69a3ba23b256c029a92b7af0d724442064d
Yash Bele <yashgbele@gmail.com>
2024-01-26 17:39:12 +0530

    hide og func

--- src/app/(news)/lifestyle/[id]/opengraph-image.js
+++ src/app/(news)/lifestyle/[id]/opengraph-image.js
@@ -1,27 +1,27 @@
-import { ImageResponse } from "next/og";
-import { lifestyle } from "@/data/lifestyle";
-import { lifestyleHistory } from "@/history/lifestyle";
-const formatData = [...lifestyle, ...lifestyleHistory];
+// import { ImageResponse } from "next/og";
+// import { lifestyle } from "@/data/lifestyle";
+// import { lifestyleHistory } from "@/history/lifestyle";
+// const formatData = [...lifestyle, ...lifestyleHistory];
 
-export const size = {
-  width: 900,
-  height: 450,
-};
+// export const size = {
+//   width: 900,
+//   height: 450,
+// };
 
-export const contentType = "image/png";
+// export const contentType = "image/png";
 
-export default async function og({ params }) {
-  const datum = formatData.find((i) => i.id === params.id);
+// export default async function og({ params }) {
+//   const datum = formatData.find((i) => i.id === params.id);
 
-  return new ImageResponse(
-    (
-      <div tw="relative flex items-center justify-center">
-        <img
-          src={`https://www.briefism.com/_next/image?url=${datum.image}&w=1920&q=75`}
-          alt={datum.id}
-        />
-      </div>
-    ),
-    size
-  );
-}
+//   return new ImageResponse(
+//     (
+//       <div tw="relative flex items-center justify-center">
+//         <img
+//           src={`https://www.briefism.com/_next/image?url=${datum.image}&w=1920&q=75`}
+//           alt={datum.id}
+//         />
+//       </div>
+//     ),
+//     size
+//   );
+// }
--- src/app/(news)/technology/[id]/opengraph-image.js
+++ src/app/(news)/technology/[id]/opengraph-image.js
@@ -1,27 +1,27 @@
-import { ImageResponse } from "next/og";
-import { technology } from "@/data/technology";
-import { technologyHistory } from "@/history/technology";
-const formatData = [...technology, ...technologyHistory];
+// import { ImageResponse } from "next/og";
+// import { technology } from "@/data/technology";
+// import { technologyHistory } from "@/history/technology";
+// const formatData = [...technology, ...technologyHistory];
 
-export const size = {
-  width: 900,
-  height: 450,
-};
+// export const size = {
+//   width: 900,
+//   height: 450,
+// };
 
-export const contentType = "image/png";
+// export const contentType = "image/png";
 
-export default async function og({ params }) {
-  const datum = formatData.find((i) => i.id === params.id);
+// export default async function og({ params }) {
+//   const datum = formatData.find((i) => i.id === params.id);
 
-  return new ImageResponse(
-    (
-      <div tw="relative flex items-center justify-center">
-        <img
-          src={`https://www.briefism.com/_next/image?url=${datum.image}&w=1920&q=75`}
-          alt={datum.id}
-        />
-      </div>
-    ),
-    size
-  );
-}
+//   return new ImageResponse(
+//     (
+//       <div tw="relative flex items-center justify-center">
+//         <img
+//           src={`https://www.briefism.com/_next/image?url=${datum.image}&w=1920&q=75`}
+//           alt={datum.id}
+//         />
+//       </div>
+//     ),
+//     size
+//   );
+// }
