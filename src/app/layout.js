dfd2a02aca6de414dc7cd24fb1df46b54ddc6b10
Yash Bele <yashgbele@gmail.com>
2024-01-29 01:49:37 -0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,11 +23,11 @@ export default function RootLayout({ children }) {
           {children}
         </main>
         <Footer />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9578547467175156"
+        />
       </body>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9578547467175156"
-      />
     </html>
   );
 }
