0c762c2393dbc1e327ef90049172f5cecd745f6e
Yash Bele <yashgbele@gmail.com>
2024-04-09 22:19:38 -0400

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,20 +18,20 @@ const montserrat = Montserrat({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <body className={`text-slate-700 select-none ${montserrat.className}`}>
+        <>
+          <Navbar />
+          <main className="max-w-7xl mx-auto mt-[calc(48px+20px)] px-5 lg:px-0">
+            {children}
+          </main>
+          <Footer />
+        </>
         <Script
           async
           strategy="lazyOnload"
           crossOrigin="anonymous"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9578547467175156"
         />
-      </head>
-      <body className={`text-slate-700 select-none ${montserrat.className}`}>
-        <Navbar />
-        <main className="max-w-7xl mx-auto mt-[calc(48px+20px)] px-5 lg:px-0">
-          {children}
-        </main>
-        <Footer />
       </body>
     </html>
   );
