a79c1e04449d7ec0d2758576b763aae9a2baf744
Yash Bele <yashgbele@gmail.com>
2024-04-23 07:44:38 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-import Script from "next/script";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
@@ -18,20 +17,19 @@ const montserrat = Montserrat({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          // async
-          // strategy="lazyOnload"
-          // crossOrigin="anonymous"
+      <body className={`text-slate-700 select-none ${montserrat.className}`}>
+        <>
+          <Navbar />
+          <main className="max-w-7xl mx-auto mt-[calc(48px+20px)] px-5 lg:px-0">
+            {children}
+          </main>
+          <Footer />
+        </>
+        <script
+          async
+          crossOrigin="anonymous"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9578547467175156"
         />
-      </head>
-      <body className={`text-slate-700 select-none ${montserrat.className}`}>
-        <Navbar />
-        <main className="max-w-7xl mx-auto mt-[calc(48px+20px)] px-5 lg:px-0">
-          {children}
-        </main>
-        <Footer />
       </body>
     </html>
   );
