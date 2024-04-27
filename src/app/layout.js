49daeaf520bcfe0b3b6028b736b804fd866cf73c
Yash Bele <yashgbele@gmail.com>
2024-04-27 07:13:50 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
-import { Montserrat } from "next/font/google";
+// import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 export const metadata = {
   title: "Latest News, Entertainment, Technology and More | Briefism",
@@ -9,15 +10,15 @@ export const metadata = {
     "Stay informed with Briefism. Get the latest news in a nutshell, delivered with clarity and brevity.",
 };
 
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  display: "block",
-});
+// const montserrat = Montserrat({
+//   subsets: ["latin"],
+//   display: "block",
+// }); //  ${montserrat.className} in body className
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`text-slate-700 select-none ${montserrat.className}`}>
+      <body className={`text-slate-700 select-none`}>
         <>
           <Navbar />
           <main className="max-w-7xl mx-auto mt-[calc(48px+20px)] px-5 lg:px-0">
@@ -25,7 +26,7 @@ export default function RootLayout({ children }) {
           </main>
           <Footer />
         </>
-        <script
+        <Script
           async
           crossOrigin="anonymous"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9578547467175156"
